4fcffb5043fe1429d3f2f704cb1227c324861cff
rockwolf <thereisanewway@gmail.com>
2013-06-07 12:44:31 +0200

    Fixed header.

--- function.py
+++ function.py
@@ -1,17 +1,5 @@
 """
-This file is part of Lisa.
-
-Lisa is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Lisa is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with Lisa. If not, see <http://www.gnu.org/licenses/>.
+See LICENSE file for copyright and license details.
 """
 from datetime import datetime
 
