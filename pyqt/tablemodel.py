e740d68c3af55d699fbaed5fcf979efe8b877ec7
rockwolf <thereisanewway@gmail.com>
2013-10-09 14:48:36 +0200

    Move lisa tablemodel data to generic.
    Also renamed the class.

--- pyqt/tablemodel.py
+++ pyqt/tablemodel.py
@@ -1,73 +1,107 @@
-#/usr/env/python
-from PyQt4.QtGui import QTableWidget, QTableWidgetItem
+#/usr/bin/env python
+from PyQt4 import QtCore, QtGui, uic
+import sys
 
-class TableModel(QTableWidget): 
-    """ Model for a table. """
+class TableModel(QtCore.QAbstractTableModel):
+    """
+        Table model for a TableView, generic reusable code
+    """
 
-    def __init__(self, header, data, *args):
-        """ Initialize the table. """
-        QTableWidget.__init__(self, *args)
-        self.header = header 
-        self.tablecontent = data
-        self.setHorizontalHeaderLabels(header)
-        self.refresh()
-       
-    def refresh(self):
-        """ Display the data as a 2D list and format + display it in the table """
-        nrows = len(self.tablecontent)
-        if nrows > 0:
-            ncols = len(self.tablecontent[0])
-        else:
-            ncols = 0;
-        self.setRowCount(nrows)
-        self.setColumnCount(ncols)
-        self.setHorizontalHeaderLabels(self.header)
-        rows = self.tablecontent
-        for row in range(len(rows)):
-            cols = self.list_to_qtablewidgetitems(rows[row])
-            for col in range(len(cols)):
-                self.setItem(row, col, cols[col])
+    def __init__(self, data = [[]], headers = [], parent = None):
+        QtCore.QAbstractTableModel.__init__(self, parent)
+        self.__values = data
+        self.__headers = headers
+   
+    def headerData(self, section, orientation, role):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                if section < len(self.__headers):
+                    return self.__headers[section]
+                else:
+                    return "TEMP"
+            else:
+                return "{}".format(section)
 
-            self.setRowHeight(row, 18)
-            # set col width first time through
-            if row == 0:
-                self.resizeColumnsToContents()
-                #self.setColumnWidth(4,250)
+    def rowCount(self, parent):
+        return len(self.__values)
 
-        # format col width
-        self.resizeColumnsToContents()
-        #self.setColumnWidth(4,250)
+    def columnCount(self, parent):
+        return len(self.__values[0])
 
+    def data(self, index, role):
+        if role == QtCore.Qt.EditRole:
+            row = index.row()
+            column = index.column()
+            return self.__values[row][column]
+        if role == QtCore.Qt.ToolTipRole:
+            row = index.row()
+            column = index.column()
+            return "data: " + self.__values[row][column]
+        #if role == QtCore.Qt.DecorationRole:
+        # row = index.row()
+        # column = index.column()
+        # value = self.__values[row][column]
+        # pixmap = QtGui.QPixmap(26, 26)
+        # pixmap.fill(value)
+        # icon = QtGui.QIcon(pixmap)
+        # return icon
+        if role == QtCore.Qt.DisplayRole:
+            row = index.row()
+            column = index.column()
+            value = self.__values[row][column]
+            return value
+   
+    def flags(self, index):
+        return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
+    def setData(self, index, value, role = QtCore.Qt.EditRole):
+        print("-- test [setData]:", value)
+        if role == QtCore.Qt.EditRole:
+            row = index.row()
+            column = index.column()
+            #if value.isValid():
+            print(value)
+            self.__values[row][column] = value
+            self.dataChanged.emit(index, index)
+            return True
+        return False
+    
+    def insertRows(self, position, rows, values = [], parent = QtCore.QModelIndex()):
+        print("-- test [insertRows]:", values)
+        self.beginInsertRows(QtCore.QModelIndex(), position, position + rows - 1)
+        for i in range(rows):
+            #for item in values[i]:
+            #print("-- test [insertRows i={}]: {}".format(i, values[i]))
+            #defaultValues = ["x" for i in range(self.columnCount(None))]
+            #self.__values.insert(position, defaultValues)
+            self.__values.insert(position, values[i])
+        self.endInsertRows()
+        return True
+
+    def insertColumns(self, position, columns, values = [], parent = QtCore.QModelIndex()):
+        self.beginInsertColumns(QtCore.QModelIndex(), position, position + columns - 1)
+        row_count = len(self.__values)
+        for i in range(columns):
+            for j in range(row_count):
+                self.__values[j].insert(position, values[j])
+        self.endInsertColumns()
+        return True
+
+    def removeRows(self, position, rows, parent = QtCore.QModelIndex()):
+        self.beginRemoveRows(QtCore.QModelIndex(), position, position + rows - 1)
+        for i in range(rows):
+            value = self.__values[position]
+            self.__values.remove(value)
+        self.endRemoveRows()
+    
     def clear(self):
-        """ Clear the table """
-        self.clearContents()
-        self.tablecontent = []
-        self.refresh()
-        
-    def list_to_qtablewidgetitems(self, list):
-        """ transforms a list in a collection of QTableWidgetItems """
-        table_items = []
-        for i in range(len(list)):
-            table_item = QTableWidgetItem(list[i])
-            table_items.append(table_item)
-        return table_items
+        """
+Clear the data.
+"""
+        self.__values = [[]]
 
-    def add_row(self, row):
-        """ insert new row """
-        #for item in data:
-        #    newitem = QTableWidgetItem(item)
-        #    self.setItem(len(self.tablecontent)+1, len(data), newitem)
-        self.tablecontent.append(row)
-        self.refresh()
-
-    def update_row(self, row, index):
-        """ update selected row with new data """
-        pass
-
-    def delete_row(self, index=-1):
-        """ delete a row on an index or just remove the last row. """
-        if index == -1:
-            self.tablecontent.pop()
-        else:
-            del self.tablecontent[index]
-        self.refresh()
+    def get_values(self):
+        """
+Returns the values.
+"""
+        return self.__values
